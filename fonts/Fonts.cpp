9825e685c816dd0ffa4ffc15a98971d6e3509335
AlexMercer <redownloader999@gmail.com>
2022-11-22 15:07:40 +0200

    MyFonts class pointers fix

--- fonts/Fonts.cpp
+++ fonts/Fonts.cpp
@@ -12,19 +12,27 @@ MyFonts::MyFonts()
 MyFonts::~MyFonts() {
   if (BisternFont) {
     delete BisternFont;
+    BisternFont = nullptr;
   }
   if (TrajanFont) {
     delete TrajanFont;
+    TrajanFont = nullptr;
   }
   if (FuturaFont) {
     delete FuturaFont;
+    FuturaFont = nullptr;
   }
   if (RobotoSlabFont) {
     delete RobotoSlabFont;
+    RobotoSlabFont = nullptr;
   }
 }
 
 sf::Font* MyFonts::GetBisternFont() {
+  /* If already created */
+  if (BisternFont) {
+    return BisternFont;
+  }
   BisternFont = new sf::Font();
   if (!BisternFont->loadFromFile("fonts/bistern/Bistern.otf")) {
     std::cout << "BisternFont font wasn't found!\n";
@@ -34,6 +42,10 @@ sf::Font* MyFonts::GetBisternFont() {
 }
 
 sf::Font* MyFonts::GetTrajanFont() {
+  /* If already created */
+  if (TrajanFont) {
+    return TrajanFont;
+  }
   TrajanFont = new sf::Font();
   if (!TrajanFont->loadFromFile("fonts/Trajan/Trajan Pro.ttf")) {
     std::cout << "TrajanFont font wasn't found!\n";
@@ -43,6 +55,10 @@ sf::Font* MyFonts::GetTrajanFont() {
 }
 
 sf::Font* MyFonts::GetFuturaFont() {
+  /* If already created */
+  if (FuturaFont) {
+    return FuturaFont;
+  }
   FuturaFont = new sf::Font();
   if (!FuturaFont->loadFromFile("fonts/Futura/futur.ttf")) {
     std::cout << "FuturaFont font wasn't found!\n";
@@ -53,6 +69,11 @@ sf::Font* MyFonts::GetFuturaFont() {
 }
 
 sf::Font* MyFonts::GetRobotoSlabFont() {
+  /* If already created */
+  if (RobotoSlabFont) {
+    return RobotoSlabFont;
+  }
+  /* if first time creating */
   RobotoSlabFont = new sf::Font();
   if (!RobotoSlabFont->loadFromFile(
           "fonts/RobotoSlab/RobotoSlab-VariableFont_wght.ttf")) {
